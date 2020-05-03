2379c38c0ebd315c3a356191e11e1e7969301cec
NyanMaths <memetech.mail@gmail.com>
2020-05-03 13:26:16 +0200

    Change sources tree (#1)
    * Deleted useless file
    
    * Deleted useless file
    
    * Deleted useless file
    
    * Deleted useless file
    
    * Delete resources.rc
    
    * Reorganized files tree
    
    Source code is in the folder "Sources".
    The files and folder to put next to the main application are in the folder "Release Files".
    
    * Info file to build MEncryptor

---
+++ Sources/MEncryptor.pro
@@ -0,0 +1,13 @@
+QT += widgets
+
+
+HEADERS += \
+    MEncryptor.h
+
+SOURCES += \
+    MEncryptor.cpp \
+    main.cpp
+
+RC_FILE = resources.rc
+
+TRANSLATIONS = MEncryptor_fr.ts
---
+++ Sources/mencryptor_fr.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MEncryptor</name>
+    <message>
+        <location filename="MEncryptor.cpp" line="26"/>
+        <source>Encrypt</source>
+        <translation>Crypter</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="27"/>
+        <source>Decrypt</source>
+        <translation>Décrypter</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="48"/>
+        <source>Encrypt / Decrypt messages</source>
+        <translation>Chiffrer / Déchiffrer</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="65"/>
+        <source>Choose what to do :</source>
+        <translation>Que faire ?</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="70"/>
+        <source>Please select protocol :</source>
+        <translation>Séléctionnez un protocole :</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="82"/>
+        <source>Output</source>
+        <translation>Sortie</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="85"/>
+        <source>Translate now !</source>
+        <translation>Traduire !</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="88"/>
+        <source>Save translated message as...</source>
+        <translation>Sauvegarder la traduction</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="128"/>
+        <source>Help and settings</source>
+        <oldsource>About and settings</oldsource>
+        <translation>Aide et paramètres</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="169"/>
+        <source>Language changed</source>
+        <translation>Langue de l&apos;interface changée</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="169"/>
+        <source>Do you want to restart the app now ?</source>
+        <translation>Voulez-vous redémarrer l&apos;application maintenant ?</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="229"/>
+        <source>
+Check in &quot;Help and settings&quot; tab to fix it !</source>
+        <translation>
+Consultez l&apos;onglet &quot;Aide et paramètres&quot; pour réparer le problème !</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="423"/>
+        <source>Shift cipher</source>
+        <translation>Chiffrement par décalage</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="423"/>
+        <location filename="MEncryptor.cpp" line="456"/>
+        <source>Please input the shift :</source>
+        <translation>Veuillez entrer le décalage :</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="456"/>
+        <source>Shift decipher</source>
+        <translation>Déchiffrement par décalage</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="275"/>
+        <source>Save translated file as...</source>
+        <translation>Sauvegarder le fichier traduit sous...</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="275"/>
+        <source>Untitled.txt</source>
+        <translation>Sans titre.txt</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="282"/>
+        <source>Operation successful, Houston !</source>
+        <translation>Opération réussie, Houston !</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="229"/>
+        <source>Runtime error</source>
+        <translation>Erreur d&apos;éxécution</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="132"/>
+        <source>This opensource software was developed by NY4N_M4THS (MemeTech INC)&lt;br/&gt;under &lt;a href = &apos;https://gnu.org/licenses/lgpl-3.0.en.html&apos;&gt;license LGPLv3&lt;/a&gt; with the framework Qt in C++ language.&lt;br/&gt;Follow &lt;a href = &apos;https://qt.io&apos;&gt;this link&lt;/a&gt; to learn more about Qt.&lt;br/&gt;You can also visit &lt;a href = &apos;https://memetech-inc.weebly.com&apos;&gt;our website&lt;/a&gt; to check for updates,&lt;br/&gt;try other of our applications or ask for new features !&lt;br/&gt;Click &lt;a href = &apos;https://github.com/NyanMaths/MEncryptor&apos;&gt;here&lt;/a&gt; to visit the GitHub page of the project.&lt;br/&gt;&lt;br/&gt;</source>
+        <oldsource>This opensource software was developed by NY4N_M4THS (MemeTech INC)&lt;br/&gt;under &lt;a href = &apos;https://gnu.org/licenses/lgpl-3.0.en.html&apos;&gt;liscence LGPLv3&lt;/a&gt; with the framework Qt in C++ language.&lt;br/&gt;Follow &lt;a href = &apos;https://qt.io&apos;&gt;this link&lt;/a&gt; to learn more about Qt.&lt;br/&gt;You can also visit &lt;a href = &apos;https://memetech-inc.weebly.com&apos;&gt;our website&lt;/a&gt; to check for updates,&lt;br/&gt;try other of our applications or ask for new features !&lt;br/&gt;Click &lt;a href = &apos;https://github.com/NyanMaths/MEncryptor&apos;&gt;here&lt;/a&gt; to visit the GitHub page of the project.&lt;br/&gt;&lt;br/&gt;</oldsource>
+        <translation>Ce logiciel opensource a été créé par NY4N_M4THS (MemeTech INC)&lt;br/&gt;sous &lt;a href = &apos;https://gnu.org/licenses/lgpl-3.0.en.html&apos;&gt;license LGPLv3&lt;/a&gt; à l&apos;aide de Qt en langage C++.&lt;br/&gt;Suivez &lt;a href = &apos;https://qt.io&apos;&gt;ce lien&lt;/a&gt; pour en savoir plus sur Qt.&lt;br/&gt;Vous pouvez également visiter &lt;a href = &apos;https://memetech-inc-fr.weebly.com&apos;&gt;notre site web&lt;/a&gt; pour télécharger les dernières mises à jour,&lt;br/&gt;découvrir d&apos;autres applications de MemeTech, ou encore demander de nouvelles fonctionalités pour MEncryptor !&lt;br/&gt;Cliquez sur &lt;a href = &apos;https://github.com/NyanMaths/MEncryptor&apos;&gt;ce lien&lt;/a&gt; pour visiter la page GitHub du projet.&lt;br/&gt;&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="229"/>
+        <source>Error #</source>
+        <translation>Erreur #</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="232"/>
+        <source>You should enter something in the text area to translate !</source>
+        <translation>Vous devez entrer quelque chose dans la zone de texte !</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="232"/>
+        <source>Little problem...</source>
+        <translation>Petit souci...</translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="282"/>
+        <source>File </source>
+        <translation>Le fichier </translation>
+    </message>
+    <message>
+        <location filename="MEncryptor.cpp" line="282"/>
+        <source> saved !</source>
+        <translation> a été sauvegardé !</translation>
+    </message>
+</context>
+</TS>
